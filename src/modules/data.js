86a00455d3e44b8e1cd787747856a69ee84d32ba
Pedro Marques <ppmarq1@gmail.com>
2022-05-18 19:26:56 +0100

    Update files with new functionalities

--- src/modules/data.js
+++ src/modules/data.js
@@ -1,33 +1,38 @@
-// Dummy data
-const data = [
-  {
-    name: 'Name',
-    score: 85,
-  },
-  {
-    name: 'Name',
-    score: 23,
-  },
-  {
-    name: 'Name',
-    score: 55,
-  },
-  {
-    name: 'Name',
-    score: 71,
-  },
-  {
-    name: 'Name',
-    score: 142,
-  },
-  {
-    name: 'Name',
-    score: 73,
-  },
-  {
-    name: 'Name',
-    score: 42,
-  },
-];
+// Game name: Starwars
+// Game id: Id01dkjflakdjf
 
-export default data;
+import generateScores from './score-generator.js';
+
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/Id01dkjflakdjf/scores/';
+
+export const fetchData = async () => {
+  const response = await fetch(url);
+  const json = await response.json();
+
+  const sorted = json.result.sort((a, b) => {
+    let num;
+    if (parseInt(a.score, 10) > parseInt(b.score, 10)) {
+      num = -1;
+    } else if (parseInt(a.score, 10) < parseInt(b.score, 10)) {
+      num = 1;
+    } else {
+      num = 0;
+    }
+    return num;
+  });
+
+  generateScores(sorted);
+};
+
+export const postData = (_name, _score) => {
+  fetch(url, {
+    method: 'POST',
+    body: JSON.stringify({
+      user: _name,
+      score: _score,
+    }),
+    headers: {
+      'Content-type': 'application/json; charset=UTF-8',
+    },
+  });
+};
